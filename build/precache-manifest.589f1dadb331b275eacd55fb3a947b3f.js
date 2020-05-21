8a44b8504600192051ca5c86f27bbe40dd947b8e
Kargamborgh <niilo.korkiakoski@gmail.com>
2020-05-21 18:52:25 +0300

    backend frontend boom boom boom

---
+++ build/precache-manifest.589f1dadb331b275eacd55fb3a947b3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a31e0850429fedfc735ecbb5ba270bc0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb14129c8ec92b11d84e",
+    "url": "/static/css/main.57a9ee58.chunk.css"
+  },
+  {
+    "revision": "acd00133a15f7dca9d20",
+    "url": "/static/js/2.d5f5676f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d5f5676f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb14129c8ec92b11d84e",
+    "url": "/static/js/main.d399720d.chunk.js"
+  },
+  {
+    "revision": "302265f7182d7e7fde30",
+    "url": "/static/js/runtime-main.b20c1ea9.js"
+  }
+]);
